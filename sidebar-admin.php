7867deb50ade2be0ca6d4452fdbd63e924176d51
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2020-06-13 00:36:42 +0700

    login & register

---
+++ sidebar-admin.php
@@ -0,0 +1,62 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <div class="sidebar-brand-icon rotate-n-15">
+    bG
+  </div>
+  <div class="sidebar-brand-text mx-3">BANGALON</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+  <a class="nav-link" href="dashboard-admin.php">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <span>Dashboard</span></a>
+</li>
+
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    <i class="fas fa-user fa-cog"></i>
+    <span>Akun</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Akun</h6>
+      <a class="collapse-item" href="akun-admin.php">Admin</a>
+      <a class="collapse-item" href="akun-pengguna.php">Pengguna</a>
+      <a class="collapse-item" href="akun-kurir.php">Kurir</a>
+    </div>
+  </div>
+</li>
+
+<!-- Nav Item - Pesanan -->
+<li class="nav-item">
+  <a class="nav-link" href="pesanan.php">
+    <i class="fas fa-shopping-cart"></i>
+    <span>Pesanan</span></a>
+</li>
+
+<!-- Nav Item - Stok Barang -->
+<li class="nav-item">
+  <a class="nav-link" href="pesanan.php">
+    <i class="fas fa-boxes"></i>
+    <span>Stok Barang</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
